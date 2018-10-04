e9b49910b60d8e9e9c7eed071402d8eda0381c93
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-10-04 10:20:48 -0600

    pseudocoding

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//SET win counter to zero
+//SET loss counter to zero
+//SET guess counter to number
+//CREATE array of possible guesses
+//CREATE method for random letter choice
+//TRACK user guess
+    //incorrect
+    //correct
+//INCREMENT appropriate counter until win or loss
+//RESET game
+
