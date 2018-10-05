5c613333f0cc4a7ca985de3ca2ee746a8d3d7e1d
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-10-04 22:06:16 -0600

    add event listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,35 +3,52 @@ var winCounter = 0;
 //SET loss counter to zero
 var lossCounter = 0;
 //SET guess counter to number
-// var guessCounter = 10;
+var guessCounter = 10;
 //CREATE array of possible guesses
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//CREATE method for random letter choice
-var activeLetter = computerChoices[Math.floor(Math.random()*computerChoices.length)];
 //CREATE var to hold guessed letters
 var guessedLetters = []
+
+//CREATE method for random letter choice
+var activeLetter = function() {
+    return computerChoices[Math.floor(Math.random()*computerChoices.length)];
+   
+}
+// console.log(activeLetter)
+
+
+//Create function to push guessed letters to array
+
+
+
+
+
+
+
+
 
 
 document.addEventListener("keyup", function(){
     var userGuess = event.key.toLowerCase();
-
-    for (var guessCounter = 10; guessCounter > -1; guessCounter--)
     
     if (userGuess !== activeLetter) {
-        
+        guessCounter--
+        console.log(guessCounter);
+        if(guessCounter === 0) {
+            console.log("Sorry, you lost")
+        }
+       
         // guessed.appendChild(guessCounter);
     }
-    else if (){
+    else{
         winCounter++
         wins.textContent = "Wins: " + winCounter;
-        
+        console.log(winCounter);
     }
-    else {
+})
 
-    }
-
-    console.log(guessCounter);
-});
+    
+// });
 //TRACK user guess
 
     //incorrect
@@ -39,4 +56,8 @@ document.addEventListener("keyup", function(){
 //INCREMENT appropriate counter until win or loss
     //WRITE information to page
 //RESET game
+    //clear the guessed letters
+    //reset the guess counter
+    //run the function to start the game again
+
 
